4fcbbfa6bc4c67dd8b187bad1f1ff00350cf586b
Phrank Lenoci <fcl1118@live.com>
2020-06-04 13:17:37 -0500

    Updates

---
+++ precache-manifest.4072cf385b53338e82ed2792fe924d44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07f1170fee6c35e599ea947aff1c920b",
+    "url": "/ReactSudoku/index.html"
+  },
+  {
+    "revision": "0b3a5f8b41f838d96a2f",
+    "url": "/ReactSudoku/static/css/main.4dc9baea.chunk.css"
+  },
+  {
+    "revision": "3e3eb7aa06553ef7b839",
+    "url": "/ReactSudoku/static/js/2.bf73afe7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactSudoku/static/js/2.bf73afe7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b3a5f8b41f838d96a2f",
+    "url": "/ReactSudoku/static/js/main.86b70f3e.chunk.js"
+  },
+  {
+    "revision": "6fc206eca8b91a36a565",
+    "url": "/ReactSudoku/static/js/runtime-main.d95aede5.js"
+  }
+]);
